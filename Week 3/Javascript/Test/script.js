c307f564f92e3c511f640dc79a857a5cf7ec2075
Burak Aydin <burak.aydin28@hotmail.com>
2022-10-11 13:11:47 +0200

    Week 3
    JS

---
+++ Week 3/Javascript/Test/script.js
@@ -0,0 +1,67 @@
+// var firstName = "John";
+// var age = 28;
+
+// console.log(firstName + " " + age);
+
+// var job, isMarried;
+// job = "teacher";
+// isMarried = false;
+
+// console.log(
+//   firstName +
+//     " is a " +
+//     age +
+//     " year old " +
+//     job +
+//     ". Is he married? " +
+//     isMarried
+// );
+
+// //variable mutation
+// age = "twenty eight";
+// job = "driver";
+
+// alert(
+//   firstName +
+//     " is a " +
+//     age +
+//     " year old " +
+//     job +
+//     ". Is he married? " +
+//     isMarried
+// );
+
+// var lastName = prompt("What is his last Name?");
+// console.log(firstName + " " + lastName);
+
+/* Basic operators */
+
+// var year, yearJohn, yearMark;
+// now = 2018;
+// ageJohn = 28;
+// ageMark = 33;
+
+// // Math operators
+// yearJohn = now - ageJohn;
+// yearMark = now - ageMark;
+
+// console.log(yearJohn);
+// console.log(yearMark);
+
+// console.log(now + 2);
+
+// //Logical operators
+// var johnOlder = ageJohn > ageMark;
+// console.log(johnOlder);
+
+// console.log(typeof johnOlder);
+// console.log(typeof ageJohn);
+// console.log(typeof "Mark is older than John");
+
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
+
+var isFullAge = now - yearJohn >= fullAge;
+
+console.log(isFullAge);
