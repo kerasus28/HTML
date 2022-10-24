889f7f3af56c6d391f82ff8470ae8cde848e4710
Burak Aydin <burak.aydin28@hotmail.com>
2022-10-24 16:16:29 +0200

    Week 3 vaardigheidsopdrachten
    pauze

--- Week 3/Javascript/Vaardigheidsopdrachten 17t:m19/script.js
+++ Week 3/Javascript/Vaardigheidsopdrachten 17t:m19/script.js
@@ -1,22 +1,59 @@
-var firstName = "Burak ";
-var lastName = "Aydin ";
-var age = 21;
-var thisYear = 2069;
-var birthYear = 2001;
+/*************
+ * 1 tm 5
+ */
+// var firstName = "Burak ";
+// var lastName = "Aydin ";
+// var age = 21;
+// var thisYear = 2069;
+// var birthYear = 2001;
 
-myAge = thisYear - birthYear;
+// myAge = thisYear - birthYear;
 
-console.log(firstName + lastName + age);
+// console.log(firstName + lastName + age);
 
-console.log("Mijn naam is " + firstName);
+// console.log("Mijn naam is " + firstName);
 
-console.log("Mijn naam volledige naam is " + firstName + lastName);
+// console.log("Mijn naam volledige naam is " + firstName + lastName);
 
-console.log("In " + thisYear + " ben ik " + myAge);
+// console.log("In " + thisYear + " ben ik " + myAge);
 
-console.log(
-  "Mijn naam is " +
-    firstName +
-    "en ik ben 34 jaar oud. Daarnaast is mijn familienaam " +
-    lastName
-);
+// console.log(
+//   "Mijn naam is " +
+//     firstName +
+//     "en ik ben 34 jaar oud. Daarnaast is mijn familienaam " +
+//     lastName
+// );
+
+/*************
+ * 6 tm 9
+ */
+
+// const dayOfTheWeek = "raining";
+// if (dayOfTheWeek === "raining") {
+//   console.log("Neem je paraplu mee!");
+// } else {
+//   console.log("Neem je zonnebril mee");
+// }
+
+// const gras = "yellow";
+// if (gras === "yellow") {
+//   console.log("Geef het water");
+// } else {
+//   console.log("Lekker niks doen");
+// }
+
+// const season = "winter";
+// if (season === "winter") {
+//   console.log("Verwarming aanzetten");
+// } else {
+//   console.log("Dansen op tafel g");
+// }
+
+// const dayOfTheWeek = "storm";
+// if (dayOfTheWeek === "storm") {
+//   console.log("Regenpak aandoen");
+// } else if (dayOfTheWeek === "raining") {
+//   console.log("paraplu meenemen");
+// } else {
+//   console.log("Zonnebril meenemen");
+// }
---
+++ Week 3/Javascript/Vaardigheidsopdrachten 20t:m25/script.js
@@ -0,0 +1,75 @@
+// const x = 1;
+// const y = "1";
+// const z = 2;
+
+// y == 1;
+
+// console.log(x + y + z);
+
+// const weather = "raining";
+
+// if (weather === "raining") {
+//   console.log("Neem een paraplu mee naar buiten");
+// }
+
+// const weather = "raining";
+
+// if (weather === "raining") {
+//   console.log("Neem een paraplu mee naar buiten");
+// } else if (weather === "sun") {
+//   console.log("Neem je zonnebril mee naar buiten");
+// } else {
+//   console.log("Neem je goede humeur maar mee");
+// }
+
+// const weather = "raining";
+
+// if (weather === "raining") {
+//   console.log("Neem een paraplu mee naar buiten");
+// } else if (weather === "sun") {
+//   console.log("Neem je zonnebril mee naar buiten");
+// }
+
+// const dayOfTheWeek = "monday";
+
+// if (dayOfTheWeek === "monday") {
+//   console.log("Je moet gaan Kickboksen");
+// } else if (dayOfTheWeek === "tuesday") {
+//   console.log("Vandaag moet je gaan boksen");
+// } else if (dayOfTheWeek === "wednesday") {
+//   console.log("Lekker patat eten");
+// } else if (dayOfTheWeek === "thursday") {
+//   console.log("BJJ");
+// } else if (dayOfTheWeek === "friday") {
+//   console.log("je moet weer naar BJJ");
+// } else {
+//   console.log("lekker patat eten");
+// }
+
+const cijfer1 = 9.4;
+const cijfer2 = 3.8;
+const cijfer3 = 2.7;
+const cijfer4 = 6.7;
+const cijfer5 = 8.9;
+
+cijferSofie = (cijfer1 + cijfer2 + cijfer3 + cijfer4 + cijfer5 * 2) / 6;
+
+// console.log(cijferSofie);
+
+const cijfer6 = 7.7;
+const cijfer7 = 7.6;
+const cijfer8 = 7.3;
+const cijfer9 = 5.8;
+const cijfer10 = 6.9;
+
+cijferSarah = (cijfer6 + cijfer7 + cijfer8 + cijfer9 + cijfer10) / 5;
+
+// console.log(cijferSarah);
+
+if (cijferSarah > cijferSofie) {
+  console.log("Gemiddelde van Sarah is hoger " + cijferSarah);
+} else if (cijferSofie > cijferSarah) {
+  console.log("Gemiddelde van Sofie is hoger " + cijferSofie);
+} else if (cijferSarah > cijferSofie || cijferSarah >= 7) {
+  console.log("help " + cijferSarah);
+}
