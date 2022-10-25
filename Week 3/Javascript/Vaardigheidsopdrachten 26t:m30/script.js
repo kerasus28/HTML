ca3a2e6f4dffa902565ac4554f2695235af61cd0
Burak Aydin <burak.aydin28@hotmail.com>
2022-10-25 13:37:56 +0200

    Week 3 JS
    Vaardigheidsopdrachten af

---
+++ Week 3/Javascript/Vaardigheidsopdrachten 26t:m30/script.js
@@ -0,0 +1,173 @@
+// const food = "kerst";
+// switch (food) {
+//   case "maandag":
+//     console.log("Falafel met salade");
+//     break;
+//   case "dinsdag":
+//     console.log("Spinazie");
+//     break;
+//   case "woensdag":
+//     console.log("Spinazie");
+//     break;
+//   case "donderdag":
+//     console.log("Falafel met salade");
+//     break;
+//   case "vrijdag":
+//     console.log("Falafel met salade");
+//     break;
+//   case "zaterdag":
+//     console.log("Linzensoep");
+//     break;
+//   case "zondag":
+//     console.log("Linzensoep");
+//     break;
+//   case "kerst":
+//     console.log("Kalkoen");
+//     break;
+// }
+
+// const car = "Opel";
+
+// if (car == "Audi") {
+//   console.log("Great");
+// } else if (car == "Mercedes") {
+//   console.log("Bit great");
+// } else if (car == "BMW") {
+//   console.log("Hmmm...");
+// } else if (car == "Opel") {
+//   console.log("OK?");
+// } else {
+//   console.log("Not so great");
+// }
+
+// const car = "Porsche";
+
+// if (car == "Audi" || car == "Porsche") {
+//   console.log("Great");
+// } else if (car == "Mercedes") {
+//   console.log("Bit great");
+// } else if (car == "BMW") {
+//   console.log("Hmmm...");
+// } else if (car == "Opel") {
+//   console.log("OK?");
+// } else {
+//   console.log("Not so great");
+// }
+
+// const car = "Opel";
+
+// if (car == "Audi" || car == "Porsche") {
+//   console.log("Great");
+// } else if (car == "Mercedes") {
+//   console.log("Bit great");
+// } else if (car == "BMW") {
+//   console.log("Hmmm...");
+// } else {
+//   console.log("Not so great!");
+// }
+
+// const job = "";
+
+// switch (job) {
+//   case "Developer":
+//     console.log("Like");
+//     break;
+//   case "Teacher":
+//     console.log("Nice.....");
+//     break;
+//   case "Consultant":
+//     console.log("Nice....");
+//     break;
+//   default:
+//     console.log("I don't know");
+// }
+
+// const job = "Retired";
+
+// switch (job) {
+//   case "Developer":
+//     console.log("LIKE");
+//     break;
+//   case "Retired":
+//     console.log("LIKE");
+//     break;
+//   case "Teacher":
+//     console.log("Nice.....");
+//     break;
+//   case "Consultant":
+//     console.log("Nice....");
+//     break;
+//   default:
+//     console.log("I don't know");
+// }
+
+// let day = "monday";
+// let message;
+
+// message = day == "monday" ? "Patat" : "Pannenkoeken";
+
+// console.log(message);
+
+// const car = "audi";
+
+// if (car == "audi") {
+//   console.log("Great");
+// } else {
+//   console.log("Mehhh");
+// }
+
+// let sentence = 10;
+
+// if (sentence == 5 > 12) {
+//   console.log("Whieee");
+// } else {
+//   console.log("Whaaaaa");
+// }
+
+// let sentence = 15;
+
+// if (sentence > 12) {
+//   console.log("Whieee");
+// } else {
+//   console.log("Whaaaaa");
+// }
+
+// let sentence = false;
+
+// if (sentence == false) {
+//   console.log("Whieee");
+// } else {
+//   console.log("Whaaaa");
+// }
+
+// let sentence = false;
+
+// if (sentence == false) {
+//   console.log("Hello, I shout out the sentence Whieeee and I am happy");
+// } else {
+//   console.log("Hello, I shout out the sentence Whaaaa and I am not so happy");
+// }
+
+// let city = "Almere";
+// let message;
+
+// message =
+//   city == "Amsterdam"
+//     ? "Koetjekoe"
+//     : `deze prachtige stad is het niet: ${city} `;
+
+// console.log(message);
+
+// let city = "Amsterdam";
+// let message;
+
+// message = city == "Amsterdam" ? "Koetjekoe" : "Koetjekoe";
+
+// console.log(message);
+
+// let city = "Amsterdam";
+// let message;
+
+// message = city == "Amsterdam" && true ? "Koetjekoe" : "Koetjeboeboe";
+
+// console.log(message);
