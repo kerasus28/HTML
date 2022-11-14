31b4e5ba58a752e8ddb205c8a08be393450f646b
Burak Aydin <burak.aydin28@hotmail.com>
2022-11-14 17:56:31 +0100

    Week 3 JS

--- JS/Week 3/Script.js
+++ JS/Week 3/Script.js
@@ -26,3 +26,89 @@
 //   const a = som1 * som2 * som3 * som4;
 //   console.log(a);
 // }
+
+// tekst(3, 6);
+
+// function tekst(som1, som2) {
+//   const a = som1 > som2;
+//   const b = som2 > som1;
+//   console.log(a, b);
+// }
+
+// Number(600, 1000);
+
+// function Number(givenNumber, number) {
+//   const a = givenNumber > number;
+//   console.log(a);
+// }
+
+// tekst();
+
+// function tekst(som1, som2) {
+//   const a = "Ik ben een koe";
+//   console.log(a);
+// }
+
+// tekst();
+
+// function tekst() {
+//   const a = "Ik ben een";
+//   // console.log(a);
+
+//   dier();
+
+//   function dier() {
+//     const b = "Schaap";
+//     console.log(a, b);
+//   }
+// }
+
+//* The name is Fritz an I am okish and my salary is 3000 and it just got doubled *//
+
+// doMath(2, 2);
+
+// function doMath(x, y) {
+//   first();
+//   function first() {
+//     const numberOne = x * 3;
+//     console.log(numberOne);
+//   }
+
+//   second();
+//   function second() {
+//     const numberTwo = y * 4;
+//     console.log(numberTwo);
+//   }
+// }
+
+// const arrayOne = ["Konijn", "Koe", "Herder", "Das", "Beer", "Vis"];
+
+// console.log(arrayOne[1]);
+// console.log(arrayOne[4]);
+// console.log("Dier 1: " + arrayOne[1] + " dier 2: " + arrayOne[2]);
+
+// console.log("Dier 1: " + arrayOne[1]);
+
+// console.log("Dier 2: " + arrayOne[2]);
+
+// console.log("Lengte array: " + arrayOne.length);
+
+// const objectOne = {
+//   firstname: "Henk",
+//   lastname: "de Vries",
+//   age: 2021 - 1988,
+//   job: "Teacher",
+//   friends: ["Koe", "Vos"],
+//   getFullName: function () {
+//     return `${this.firstname} ${this.lastname}`;
+//   },
+// };
+
+// console.log("My job is being a " + objectOne.job);
+// console.log(
+//   "My job is being a " +
+//     objectOne.job +
+//     " and I am " +
+//     objectOne.age +
+//     " Years old"
+// );
