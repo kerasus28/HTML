a558ffbb8b5f115166c6fc45eb8f7dd1fd3d3ae5
Burak Aydin <burak.aydin28@hotmail.com>
2022-11-14 13:10:52 +0100

    Week 3 JS

---
+++ JS/Week 3/Script.js
@@ -0,0 +1,28 @@
+// tekst();
+
+// function tekst() {
+//   const a = "Mijn eerste functie";
+//   console.log(a);
+// }
+
+// tekst(5, 5);
+
+// function tekst(som1, som2) {
+//   const a = som1 + som2;
+//   console.log(a);
+// }
+
+// tekst(5, 1);
+
+// function tekst(som1, som2) {
+//   const a = som1 - som2;
+//   console.log(a);
+// }
+
+// tekst(4, 5, 6, 7);
+// tekst(2, 3, 4, 5);
+
+// function tekst(som1, som2, som3, som4) {
+//   const a = som1 * som2 * som3 * som4;
+//   console.log(a);
+// }
