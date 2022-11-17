6d23901588da366a710c89e009d9c3c1058d0964
Burak Aydin <burak.aydin28@hotmail.com>
2022-11-17 11:25:10 +0100

    Week 5 JS
    Bijna alle vaardigheidsopdrachten

---
+++ JS/Week 5/script.js
@@ -0,0 +1,105 @@
+// "use strict";
+//opd 1
+// console.log(document.querySelector(".grid-container_item-1"));
+
+// opd2
+
+// console.log(
+//   document.querySelector(".grid-container_item-1"),
+//   document.querySelector(".grid-container_item-2")
+// );
+
+//opd3
+
+// console.log(
+//   document.querySelector(".grid-container_item-1").textContent,
+//   document.querySelector(".grid-container_item-2").textContent
+// );
+
+//opd4
+
+// console.log(
+//   document.querySelector(".grid-container_item-1").textContent,
+//   document.querySelector(".grid-container_item-2").textContent,
+//   document.querySelector(".grid-container_item-1").textContent,
+//   document.querySelector(".grid-container_item-2").textContent
+// );
+
+//opd5
+
+// console.log(
+//   document.querySelector(".grid-container_item-1").textContent,
+//   document.querySelector(".grid-container_item-2").textContent,
+//   document.querySelector(".grid-container_item-3").textContent,
+//   document.querySelector(".grid-container_item-4").textContent
+// );
+
+//opd6
+
+// document.querySelector(".grid-container").style.textAlign = "center";
+
+// document.querySelector(".grid-container_item-1").style.backgroundColor = "red";
+// document.querySelector(".grid-container_item-2").style.backgroundColor = "blue";
+// document.querySelector(".grid-container_item-3").style.backgroundColor =
+//   "green";
+// document.querySelector(".grid-container_item-4").style.backgroundColor =
+//   "purple";
+// document.querySelector(".grid-container_item-5").style.backgroundColor =
+//   "brown";
+// document.querySelector(".grid-container_item-6").style.backgroundColor = "pink";
+// document.querySelector(".grid-container_item-7").style.backgroundColor =
+//   "yellow";
+
+//opd11
+
+// document.querySelector(".grid-container").style.textAlign = "center";
+
+// document.querySelector(".grid-container_item-1").style.backgroundColor = "red";
+// document.querySelector(".grid-container_item-2").style.backgroundColor = "blue";
+// document.querySelector(".grid-container_item-3").style.backgroundColor =
+//   "green";
+// document.querySelector(".grid-container_item-4").style.backgroundColor =
+//   "purple";
+// document.querySelector(".grid-container_item-5").style.backgroundColor =
+//   "brown";
+// document.querySelector(".grid-container_item-6").style.backgroundColor = "pink";
+// document.querySelector(".grid-container_item-7").style.backgroundColor =
+//   "yellow";
+
+// document
+//   .querySelector(".grid-container_item-1")
+//   .addEventListener("click", function handleClick() {
+//     console.log("Hello world!");
+//   });
+
+//opd12
+
+// const print = 0;
+
+// document
+//   .querySelector(".grid-container_item-5")
+//   .addEventListener("click", function handleClick() {
+//     for (let print = 0; print < 10; print++) {
+//       console.log("Hello world!");
+//     }
+//   });
+
+//opd13
+
+// document
+//   .querySelector(".grid-container_item-3")
+//   .addEventListener("click", function handleClick() {
+//     document.querySelector(".grid-container_item-3").textContent =
+//       "JSC is de beste";
+//   });
+
+//opd15
+
+// document
+//   .querySelector(".grid-container_item-5")
+//   .addEventListener("click", function handleClick() {
+//     document.querySelector(".grid-container_item-6").textContent =
+//       "Hello world!";
+//   });
+
+//opd16
